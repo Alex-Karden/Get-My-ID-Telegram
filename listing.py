4373f3128577f08bd4f1aad4d71e18a4c944c40f
Alex-Karden <61961749+Alex-Karden@users.noreply.github.com>
2025-03-03 15:42:44 +0300

    Update listing.py

--- listing.py
+++ listing.py
@@ -4,6 +4,6 @@ bot = telebot.TeleBot('BOT-TOKEN')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'  Ваш ID:  {message.from_user.id}')
+    bot.send_message(message.chat.id, f'Ваш ID: {message.from_user.id}, Ваш username: {message.from_user.username} Ваше имя: {message.from_user.first_name}, Ваша фамилия: {message.from_user.last_name}')
 
 bot.infinity_polling();
